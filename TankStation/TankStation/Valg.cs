698be46221d1474d5352cfc8910d7d4e8a3e220e
Alex4969UCL <60386253+Alex4969UCL@users.noreply.github.com>
2020-01-29 12:15:06 +0100

    Update Valg.cs

--- TankStation/TankStation/Valg.cs
+++ TankStation/TankStation/Valg.cs
@@ -8,25 +8,34 @@ namespace TankStation
     {
         public enum Brændstof { Diesel, Benzin92, Benzin95 };
 
-        public double Diesel { get; set; } = 9.47;
-        public double Benzin92 { get; set; } = 10.23;
-        public double Benzin95 { get; set; } = 10.45;
+        public static double Diesel { get; set; } = 9.47;
+        public static double Benzin92 { get; set; } = 10.23;
+        public static double Benzin95 { get; set; } = 10.45;
 
         public Brændstof Type { get; set; }
 
         public void ValgAfBrændstof()
         {
-            Console.WriteLine("Vælg type brændstof:\n 1. Diesel \n 2. Benzin92 \n 3. Benzin95");
-            if(int.TryParse(Console.ReadLine(), out int valg)) 
+            int valg = 0;
+            do
             {
-                switch (valg)
+
+                Console.WriteLine("Vælg type brændstof:\n 1. Diesel \n 2. Benzin92 \n 3. Benzin95");
+                if (int.TryParse(Console.ReadLine(), out valg))
                 {
-                    case 1: Type = Brændstof.Diesel; break;
-                    case 2: Type = Brændstof.Benzin92; break;
-                    case 3: Type = Brændstof.Benzin95; break;
+                    switch (valg)
+                    {
+                        case 1: Type = Brændstof.Diesel; break;
+                        case 2: Type = Brændstof.Benzin92; break;
+                        case 3: Type = Brændstof.Benzin95; break;
+                    }
+
                 }
-                
-            }
+                else
+                {
+                    Console.WriteLine("Ikke forstået, prøv igen");
+                }
+            } while (valg != 1 || valg != 2 || valg != 3);
         }
     }
 }

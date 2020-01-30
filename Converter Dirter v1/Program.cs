72804a2114c2db68c7b1402550ee046622b29560
Anders <ande671f@edu.ucl.dk>
2020-01-30 09:59:24 +0100

    test1 anders

---
+++ Converter Dirter v1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Coverter_Dirter;
+
+namespace Converter_Dirter_v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Weight test1 = new Weight();
+            Valuta test2 = new Valuta();
+            Length test3 = new Length();
+            Console.WriteLine(test1.ConvertKilogramToGram(10));
+            Console.WriteLine(test2.ValutaToUSD(10, "DKK"));
+            Console.WriteLine(test3.ConverterMeterFod(1));
+            Console.ReadLine();
+        }
+    }
+}

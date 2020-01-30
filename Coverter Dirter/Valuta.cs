85e2ad615b24708c632cc32cbb8e7e5a34a8e929
Mads Klejs Vilhelmsen <mads019f@edu.ucl.dk>
2020-01-30 09:31:56 +0100

    valuta
    so gooooooood!....

---
+++ Coverter Dirter/Valuta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coverter_Dirter
+{
+    public class Valuta
+    {
+        public string ValutaToUSD(double amount, string valuta)
+        {
+            string message = "";
+            double result = 0;
+
+            switch (valuta)
+            {
+                case "DKK":
+                    result = amount / 6.79;
+                    break;
+
+                case "EUR":
+                    result = amount / 0.91;
+                    break;
+
+                case "SEK":
+                    result = amount / 9.65;
+
+                    break;
+            }
+            message = $"{amount} {valuta} = {result} USD";
+
+            return message;
+        }
+        
+
+
+
+
+
+
+
+
+    }
+}

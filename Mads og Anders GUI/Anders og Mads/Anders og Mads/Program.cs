e280df66b21ccbfeee0f6f9f7fa79364ec8b50ff
Anders <ande671f@edu.ucl.dk>
2020-01-31 09:21:13 +0100

    commit

---
+++ Mads og Anders GUI/Anders og Mads/Anders og Mads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Anders_og_Mads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
